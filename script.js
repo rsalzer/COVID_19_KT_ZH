612caf4fa543a8f0cfc390ea1906e09d4e8ca311
Robert Salzer <robert.salzer@garzotto.com>
2021-01-09 18:24:32 +0100

    deploy vergleich

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ Chart.defaults.global.defaultFontFamily = "IBM Plex Sans";
 document.getElementById("loaded").style.display = 'none';
 
 //setLanguageNav();
-
+includeHTML();
 getCantonZH();
 getBezirke();
 getPLZ();
@@ -2252,3 +2252,31 @@ var plzNames = {
 8954: "Geroldswil",
 8955: "Oetwil ad. L."
 };
+
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("nav");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("url");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.insertAdjacentHTML("afterbegin", this.responseText);}
+          if (this.status == 404) {elmnt.insertAdjacentHTML("afterbegin", "Page not found.");}
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("url");
+          document.getElementById("pagenav_zh").className = "here";
+        }
+      }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
+    }
+  }
+}
